5b7077a5b7782dc3462e06205694b9ee1dbf8f89
wpbonelli <wbonelli@ucar.edu>
2025-04-28 14:13:00 -0400

    successfully parse all namefiles

--- autotest/test_codec.py
+++ autotest/test_codec.py
@@ -1,9 +1,12 @@
+from pprint import pprint
+
 import pytest
 
 import modflow_devtools.models as models
 from modflow_devtools.codec import make_parser
 
 MODELS = [name for name in models.get_models().keys() if name.startswith("mf6/")]
+PARSER = make_parser()
 
 
 @pytest.mark.parametrize("model_name", MODELS)
@@ -11,6 +14,6 @@ def test_parser(model_name, function_tmpdir):
     workspace = models.copy_to(function_tmpdir, model_name)
     nam_path = next(iter(workspace.glob("*.nam")))
     text = nam_path.open().read()
-    parser = make_parser()
-    tree = parser.parse(text)
+    pprint(text)
+    tree = PARSER.parse(text)
     print(tree.pretty())
