73611e4d2d7901ae746e2bdaa3f18ef3bac3989b
w-bonelli <wesbonelli@gmail.com>
2025-10-15 15:52:07 -0400

    fix(dfn): add missing field attributes, model type constant

--- modflow_devtools/dfn/schema/v1.py
+++ modflow_devtools/dfn/schema/v1.py
@@ -1,20 +1,43 @@
 from dataclasses import dataclass
+from typing import Literal
 
 from modflow_devtools.dfn.schema.field import Field
+
+FieldType = Literal[
+    "keyword",
+    "integer",
+    "double precision",
+    "string",
+    "record",
+    "recarray",
+    "keystring",
+]
+
+
+Reader = Literal[
+    "urword",
+    "u1ddbl",
+    "u2ddbl",
+    "readarray",
+]
 
 
 @dataclass(kw_only=True)
 class FieldV1(Field):
     valid: tuple[str, ...] | None = None
-    tagged: bool | None = None
-    in_record: bool | None = None
+    reader: Reader = "urword"
+    tagged: bool = False
+    in_record: bool = False
     layered: bool | None = None
     longname: str | None = None
-    preserve_case: bool | None = None
-    numeric_index: bool | None = None
+    preserve_case: bool = False
+    numeric_index: bool = False
     deprecated: bool = False
     removed: bool = False
     mf6internal: str | None = None
+    netcdf: str | None = None
+    block_variable: bool = False
+    just_data: bool = False
 
     @classmethod
     def from_dict(cls, d: dict, strict: bool = False) -> "FieldV1":
@@ -29,9 +52,10 @@ class FieldV1(Field):
             If True, raise ValueError if dict contains unrecognized keys.
             If False (default), ignore unrecognized keys.
         """
-        keys = list(cls.__annotations__.keys()) + list(Field.__annotations__.keys())
+        keys = set(
+            list(cls.__annotations__.keys()) + list(Field.__annotations__.keys())
+        )
         if strict:
-            extra_keys = set(d.keys()) - set(keys)
-            if extra_keys:
+            if extra_keys := set(d.keys()) - keys:
                 raise ValueError(f"Unrecognized keys in field data: {extra_keys}")
         return cls(**{k: v for k, v in d.items() if k in keys})
--- modflow_devtools/dfn/schema/v2.py
+++ modflow_devtools/dfn/schema/v2.py
@@ -25,9 +25,10 @@ class FieldV2(Field):
             If True, raise ValueError if dict contains unrecognized keys.
             If False (default), ignore unrecognized keys.
         """
-        keys = list(cls.__annotations__.keys()) + list(Field.__annotations__.keys())
+        keys = set(
+            list(cls.__annotations__.keys()) + list(Field.__annotations__.keys())
+        )
         if strict:
-            extra_keys = set(d.keys()) - set(keys)
-            if extra_keys:
+            if extra_keys := set(d.keys()) - keys:
                 raise ValueError(f"Unrecognized keys in field data: {extra_keys}")
         return cls(**{k: v for k, v in d.items() if k in keys})
